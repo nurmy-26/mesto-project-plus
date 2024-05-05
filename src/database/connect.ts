72c154b276def49ab34b4a6b84cd218a95782535
Nurmy <nurmy26@yandex.ru>
2024-05-05 15:20:32 +0300

    refactor

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -6,7 +6,7 @@ const connect = async (app: Express, url: string, port?: string | number) => {
   await mongoose.connect(url);
   await app.listen(port);
   // eslint-disable-next-line
-  console.log(`Сервер запущен и слушает на порту ${port}...`);
+  // console.log(`Сервер запущен и слушает на порту ${port}...`);
 };
 
 export default connect;
